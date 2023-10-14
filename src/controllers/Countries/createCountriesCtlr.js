7ae3bf2a89c729261b3e8a96b670016410d248a4
AlaMarti <rodrigoalanmartinez@gmail.com>
2023-10-13 22:46:04 -0300

    Ruta create terminada

--- src/controllers/Countries/createCountriesCtlr.js
+++ src/controllers/Countries/createCountriesCtlr.js
@@ -1,3 +1,25 @@
-module.exports = (newCountry) => {
-  return "POST Ha ingresado al controler para CREAR el Country " + newCountry;
-};
+const {Country} = require("../../db")
+
+const createCountries = async(name, image, description, experiences, continents ) => {
+
+  const existingCountry = await Country.findOne({
+    where : {
+      name,
+    }
+  })
+  if(existingCountry){
+    throw new Error("Ya existe el pais que deseas crear")
+  }
+
+  if(!name || !description ||!image ||!experiences ||!continents) {
+    throw Error ("Faltan datos")
+  } else{
+      const newCountry = await Country.create({name, image, description, experiences, continents})
+
+    return newCountry
+  }
+}
+
+
+module.exports = {createCountries}
+
