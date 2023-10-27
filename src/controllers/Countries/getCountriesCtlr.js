52a5407b521f9170388cd081ae904cfa5174ce5b
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2023-10-27 17:28:25 -0500

    Cambio de Countries

--- src/controllers/Countries/getCountriesCtlr.js
+++ src/controllers/Countries/getCountriesCtlr.js
@@ -8,7 +8,7 @@ const allCountries = async () => {
   }
 
   const response = countries.map((c) => {
-    return { id: c.id, name: c.name, image: c.image, continent: c.continent };
+    return { id: c.id, name: c.name, image: c.image, description: c.description, experiences: c.experiences, continent: c.continent };
   });
 
   return response;
