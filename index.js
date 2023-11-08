b11d9221d3d328a7475a1c7c1fa298624bae2fe4
puravidaviajespremium <147449744+puravidaviajespremium@users.noreply.github.com>
2023-11-08 14:35:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { PORT } = process.env;
 const { conn } = require("./src/db.js");
 
-conn.sync({ forse: false }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Listen at port ${PORT}`);
   });
